5e5da8d5e509f1718774ffb30bc8248f4b53a7ed
Aleksander <aleksander.0705@gmail.com>
2024-09-08 17:19:03 +0300

    cors = require('cors')

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ export const setupServer = () => {
   app.use(express.json());
 
   // app.use(cors());
-  // const cors = require('cors');
+  const cors = require('cors');
   app.use(cors({
     origin: env("FRONTEND_DOMAIN")
   }));
